36be23b02005d9d886db4e05584f1d032e0210c4
Ivan Krstic <krstic8687@yahoo.com>
2021-01-03 11:46:06 +0100

    Initial commit

---
+++ PresentationLayer/DataAccessLayer/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Constants
+    {
+        public static string connectionString = "user id=Z6AJNSMv02;server=remotemysql.com;persistsecurityinfo=False;database=Z6AJNSMv02";
+    }
+}
---
+++ PresentationLayer/DataAccessLayer/Models/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Admin
+    {
+        public int admin_id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public string ToString()
+        {
+            return (admin_id + " " + name + " " + surname + " " + username + " " + password);
+        }
+    }
+}
