43f12e1b4be7bae927eae1428da1141e86c89cc9
Ivan Krstic <krstic8687@yahoo.com>
2021-01-04 00:59:11 +0100

    Created connection string and defined insert and select query

---
+++ PresentationLayer/DataAccessLayer/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public class Constants
+    {
+        public static string connectionString = "server=remotemysql.com;user id=Z6AJNSMv02;database=Z6AJNSMv02;persistsecurityinfo=True";
+
+    }
+}
