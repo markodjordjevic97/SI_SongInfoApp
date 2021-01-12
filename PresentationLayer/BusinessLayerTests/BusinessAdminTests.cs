7391a9cf6f0c2c7cd16b934a9000731c43b6b194
Ivan Krstic <krstic8687@yahoo.com>
2021-01-12 17:13:37 +0100

    Integration Testing - BusinessLayer

---
+++ PresentationLayer/BusinessLayerTests/BusinessAdminTests.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BusinessLayerTests
+{
+    [TestClass]
+    public class BusinessAdminTests
+    {
+        BusinessAdmin businessAdmin;
+
+        [TestInitialize]
+        public void SetData()
+        {
+            businessAdmin = new BusinessAdmin();
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            businessAdmin.DeleteAdmin(new Admin()
+            {
+                Username = "ivankrstic"
+            });
+        }
+
+        [TestMethod]
+        public void IsAdminValidRegistered()
+        {
+            //Arrange
+            var admin = new Admin()
+            {
+                Name = "Ivan",
+                Surname = "Krstic",
+                Username = "ivankrstic",
+                Password = "Ikrstic123456"
+            };
+
+            //Act
+            int result = businessAdmin.RegisterAdmin(admin);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+    }
+}
