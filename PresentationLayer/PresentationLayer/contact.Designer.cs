bb8016c7c9466e6acb016402c7868d5dd246e248
markodjordjevic <srecanmare9@gmail.com>
2021-01-12 21:09:06 +0100

    Add text on contact

--- PresentationLayer/PresentationLayer/contact.Designer.cs
+++ PresentationLayer/PresentationLayer/contact.Designer.cs
@@ -33,18 +33,18 @@
             this.panelJovan = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.LabelJovan = new System.Windows.Forms.Label();
             this.panelIvan = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.btnIvanCenter = new Bunifu.Framework.UI.BunifuImageButton();
             this.btnIvanLeft = new Bunifu.Framework.UI.BunifuImageButton();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.btnIvanRight = new Bunifu.Framework.UI.BunifuImageButton();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.LabelIvan = new System.Windows.Forms.Label();
             this.panelMarko = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LabelMarko = new System.Windows.Forms.Label();
             this.bunifuGradientPanel1.SuspendLayout();
             this.panelJovan.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -70,9 +70,10 @@
             this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(79)))), ((int)(((byte)(86)))));
             this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
             this.bunifuGradientPanel1.Quality = 10;
-            this.bunifuGradientPanel1.Size = new System.Drawing.Size(948, 633);
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(1066, 791);
             this.bunifuGradientPanel1.TabIndex = 0;
             // 
             // panelJovan
@@ -81,24 +82,26 @@
             this.panelJovan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panelJovan.Controls.Add(this.pictureBox3);
             this.panelJovan.Controls.Add(this.label5);
-            this.panelJovan.Controls.Add(this.label6);
+            this.panelJovan.Controls.Add(this.LabelJovan);
             this.panelJovan.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelJovan.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(79)))), ((int)(((byte)(86)))));
             this.panelJovan.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelJovan.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(79)))), ((int)(((byte)(86)))));
-            this.panelJovan.Location = new System.Drawing.Point(551, 0);
+            this.panelJovan.Location = new System.Drawing.Point(620, 0);
+            this.panelJovan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelJovan.Name = "panelJovan";
             this.panelJovan.Quality = 10;
-            this.panelJovan.Size = new System.Drawing.Size(394, 633);
+            this.panelJovan.Size = new System.Drawing.Size(443, 791);
             this.panelJovan.TabIndex = 1;
             // 
             // pictureBox3
             // 
             this.pictureBox3.BackgroundImage = global::PresentationLayer.Properties.Resources.j_min;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(116, 40);
+            this.pictureBox3.Location = new System.Drawing.Point(130, 50);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(180, 186);
+            this.pictureBox3.Size = new System.Drawing.Size(202, 232);
             this.pictureBox3.TabIndex = 12;
             this.pictureBox3.TabStop = false;
             // 
@@ -108,23 +111,21 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(56, 289);
+            this.label5.Location = new System.Drawing.Point(63, 361);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(169, 25);
+            this.label5.Size = new System.Drawing.Size(196, 30);
             this.label5.TabIndex = 13;
             this.label5.Text = "Jovan Govedarica";
             // 
-            // label6
+            // LabelJovan
             // 
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(56, 328);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(300, 200);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "Loremajsdnfkjandsfkgsndkfjgskjdfngksndfkjngksdfngkjsndfkgnksdnfggskdfmgmflkmgkjsd" +
-    "fngksdfmkgmsdkfmglskdfmklgsmdfklgmsldfmgsdfgsdfgsdfgsd";
+            this.LabelJovan.BackColor = System.Drawing.Color.Transparent;
+            this.LabelJovan.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.LabelJovan.ForeColor = System.Drawing.Color.White;
+            this.LabelJovan.Location = new System.Drawing.Point(63, 410);
+            this.LabelJovan.Name = "LabelJovan";
+            this.LabelJovan.Size = new System.Drawing.Size(338, 305);
+            this.LabelJovan.TabIndex = 14;
             // 
             // panelIvan
             // 
@@ -135,15 +136,16 @@
             this.panelIvan.Controls.Add(this.pictureBox2);
             this.panelIvan.Controls.Add(this.btnIvanRight);
             this.panelIvan.Controls.Add(this.label4);
-            this.panelIvan.Controls.Add(this.label3);
+            this.panelIvan.Controls.Add(this.LabelIvan);
             this.panelIvan.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelIvan.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(79)))), ((int)(((byte)(86)))));
             this.panelIvan.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelIvan.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(79)))), ((int)(((byte)(86)))));
-            this.panelIvan.Location = new System.Drawing.Point(267, 0);
+            this.panelIvan.Location = new System.Drawing.Point(300, 0);
+            this.panelIvan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelIvan.Name = "panelIvan";
             this.panelIvan.Quality = 10;
-            this.panelIvan.Size = new System.Drawing.Size(394, 636);
+            this.panelIvan.Size = new System.Drawing.Size(443, 795);
             this.panelIvan.TabIndex = 0;
             // 
             // btnIvanCenter
@@ -152,9 +154,10 @@
             this.btnIvanCenter.ErrorImage = null;
             this.btnIvanCenter.Image = global::PresentationLayer.Properties.Resources.dot_circle_regular_min;
             this.btnIvanCenter.ImageActive = null;
-            this.btnIvanCenter.Location = new System.Drawing.Point(178, 528);
+            this.btnIvanCenter.Location = new System.Drawing.Point(200, 660);
+            this.btnIvanCenter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnIvanCenter.Name = "btnIvanCenter";
-            this.btnIvanCenter.Size = new System.Drawing.Size(38, 38);
+            this.btnIvanCenter.Size = new System.Drawing.Size(43, 48);
             this.btnIvanCenter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnIvanCenter.TabIndex = 12;
             this.btnIvanCenter.TabStop = false;
@@ -166,9 +169,10 @@
             this.btnIvanLeft.BackColor = System.Drawing.Color.Transparent;
             this.btnIvanLeft.Image = global::PresentationLayer.Properties.Resources.arrow_left_solid_min;
             this.btnIvanLeft.ImageActive = null;
-            this.btnIvanLeft.Location = new System.Drawing.Point(128, 528);
+            this.btnIvanLeft.Location = new System.Drawing.Point(144, 660);
+            this.btnIvanLeft.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnIvanLeft.Name = "btnIvanLeft";
-            this.btnIvanLeft.Size = new System.Drawing.Size(38, 38);
+            this.btnIvanLeft.Size = new System.Drawing.Size(43, 48);
             this.btnIvanLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnIvanLeft.TabIndex = 11;
             this.btnIvanLeft.TabStop = false;
@@ -179,9 +183,10 @@
             // 
             this.pictureBox2.BackgroundImage = global::PresentationLayer.Properties.Resources.i_min;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(117, 37);
+            this.pictureBox2.Location = new System.Drawing.Point(132, 46);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(169, 186);
+            this.pictureBox2.Size = new System.Drawing.Size(190, 232);
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
@@ -190,9 +195,10 @@
             this.btnIvanRight.BackColor = System.Drawing.Color.Transparent;
             this.btnIvanRight.Image = global::PresentationLayer.Properties.Resources.arrow_right_solid_min;
             this.btnIvanRight.ImageActive = null;
-            this.btnIvanRight.Location = new System.Drawing.Point(228, 528);
+            this.btnIvanRight.Location = new System.Drawing.Point(256, 660);
+            this.btnIvanRight.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnIvanRight.Name = "btnIvanRight";
-            this.btnIvanRight.Size = new System.Drawing.Size(38, 38);
+            this.btnIvanRight.Size = new System.Drawing.Size(43, 48);
             this.btnIvanRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnIvanRight.TabIndex = 10;
             this.btnIvanRight.TabStop = false;
@@ -205,23 +211,21 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(57, 286);
+            this.label4.Location = new System.Drawing.Point(64, 358);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.Size = new System.Drawing.Size(122, 30);
             this.label4.TabIndex = 8;
             this.label4.Text = "Ivan Krstić";
             // 
-            // label3
+            // LabelIvan
             // 
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(57, 325);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(300, 200);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Loremajsdnfkjandsfkgsndkfjgskjdfngksndfkjngksdfngkjsndfkgnksdnfggskdfmgmflkmgkjsd" +
-    "fngksdfmkgmsdkfmglskdfmklgsmdfklgmsldfmgsdfgsdfgsdfgsd";
+            this.LabelIvan.BackColor = System.Drawing.Color.Transparent;
+            this.LabelIvan.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.LabelIvan.ForeColor = System.Drawing.Color.White;
+            this.LabelIvan.Location = new System.Drawing.Point(64, 406);
+            this.LabelIvan.Name = "LabelIvan";
+            this.LabelIvan.Size = new System.Drawing.Size(338, 250);
+            this.LabelIvan.TabIndex = 9;
             // 
             // panelMarko
             // 
@@ -229,24 +233,26 @@
             this.panelMarko.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panelMarko.Controls.Add(this.pictureBox1);
             this.panelMarko.Controls.Add(this.label1);
-            this.panelMarko.Controls.Add(this.label2);
+            this.panelMarko.Controls.Add(this.LabelMarko);
             this.panelMarko.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelMarko.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(79)))), ((int)(((byte)(86)))));
             this.panelMarko.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelMarko.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(79)))), ((int)(((byte)(86)))));
             this.panelMarko.Location = new System.Drawing.Point(0, 0);
+            this.panelMarko.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelMarko.Name = "panelMarko";
             this.panelMarko.Quality = 10;
-            this.panelMarko.Size = new System.Drawing.Size(397, 633);
+            this.panelMarko.Size = new System.Drawing.Size(447, 791);
             this.panelMarko.TabIndex = 12;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = global::PresentationLayer.Properties.Resources.m_min;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(117, 37);
+            this.pictureBox1.Location = new System.Drawing.Point(132, 46);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(169, 186);
+            this.pictureBox1.Size = new System.Drawing.Size(190, 232);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
@@ -256,31 +262,30 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(57, 286);
+            this.label1.Location = new System.Drawing.Point(64, 358);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.Size = new System.Drawing.Size(179, 30);
             this.label1.TabIndex = 8;
             this.label1.Text = "Marko Đorđević";
             // 
-            // label2
+            // LabelMarko
             // 
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(57, 325);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(300, 200);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Loremajsdnfkjandsfkgsndkfjgskjdfngksndfkjngksdfngkjsndfkgnksdnfggskdfmgmflkmgkjsd" +
-    "fngksdfmkgmsdkfmglskdfmklgsmdfklgmsldfmgsdfgsdfgsdfgsd";
+            this.LabelMarko.BackColor = System.Drawing.Color.Transparent;
+            this.LabelMarko.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.LabelMarko.ForeColor = System.Drawing.Color.White;
+            this.LabelMarko.Location = new System.Drawing.Point(64, 406);
+            this.LabelMarko.Name = "LabelMarko";
+            this.LabelMarko.Size = new System.Drawing.Size(338, 309);
+            this.LabelMarko.TabIndex = 9;
             // 
             // contact
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.bunifuGradientPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "contact";
-            this.Size = new System.Drawing.Size(948, 633);
+            this.Size = new System.Drawing.Size(1066, 791);
             this.Load += new System.EventHandler(this.Contact_Load);
             this.bunifuGradientPanel1.ResumeLayout(false);
             this.panelJovan.ResumeLayout(false);
@@ -304,17 +309,17 @@
         private Bunifu.Framework.UI.BunifuGradientPanel panelMarko;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LabelMarko;
         private Bunifu.Framework.UI.BunifuGradientPanel panelIvan;
         private Bunifu.Framework.UI.BunifuImageButton btnIvanLeft;
         private System.Windows.Forms.PictureBox pictureBox2;
         private Bunifu.Framework.UI.BunifuImageButton btnIvanRight;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LabelIvan;
         private Bunifu.Framework.UI.BunifuGradientPanel panelJovan;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LabelJovan;
         private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
         private Bunifu.Framework.UI.BunifuImageButton btnIvanCenter;
     }
--- PresentationLayer/PresentationLayer/contact.cs
+++ PresentationLayer/PresentationLayer/contact.cs
@@ -8,6 +8,20 @@ namespace PresentationLayer
         public contact()
         {
             InitializeComponent();
+            LabelJovan.Text = "Rodjen sam 27.05.1999 " +
+                "godine u mestu Sokolac, RS, BIH. Maturirao 2018. godine sa" + " " +
+                "odlicnim uspehom. Student sam trece godine smera informacionih " +
+                "tehnologija na fakultetu tehnickih nauka u Cacku." + "\n" +
+                "Email: jocololika9@gmail.com";
+            LabelIvan.Text = "Rodjen u Beogradu 1999. godine, trenutno zivim u Gorazdevcu nadomak Peci. " +
+                "Student sam trece godine na smeru IT. " +
+                "Hobiji: sport, putovanja, muzika, filmovi. " + "\n" +
+                "Email: krstic8687@yahoo.com";
+            LabelMarko.Text = "Dolazim iz Vranja, student sam trece godine na smeru IT. " +
+                "Hobiji: Angular, stoni tenis, filmovi i muzika. " + "\n" +
+                "Iskustva: Edit Comtrade, Centar za mlade talente Novi Sad." + "\n" +
+                "Email: srecanmare9@gmail.com";
+
         }
 
         private void Contact_Load(object sender, EventArgs e)
@@ -38,5 +52,7 @@ namespace PresentationLayer
             panelMarko.SendToBack();
             panelJovan.SendToBack();
         }
+
+        
     }
 }
